e62cd4e2f9974ec8413557725407584b0e336fac
BrianSolis <Wysern94@gmail.com>
2021-10-02 15:37:51 -0500

    Full decryption code with multiple data separated with commas

--- Decrypt.cpp
+++ Decrypt.cpp
@@ -1,30 +1,55 @@
 #include <iostream>
-#include <string.h>
+#include <sstream>
 #include <gmpxx.h>
 
 using namespace std;
-using std::to_string;
-using std::stoi;
 
-int main()
+int main(int argc, char **argv)
 {
-    unsigned long n, d;
-    string message, n_str, d_str;
+
+    /*
+    mpz_t a, b, c, d;
+    mpz_inits(a, b, c, d,NULL);
+
+    mpz_set_str(a, "3", 10);
+    mpz_set_str(b, "221", 10); //Decimal base
+
+    mpz_ui_pow_ui(c, 152, 77);
+    mpz_mod(d, c, b);
+    cout << "\nThe exact result is:";
+    mpz_out_str(stdout, 10, d); //Stream, numerical base, var
+    cout << endl;
+*/
 
     mpz_t _c, _n, _d;
     mpz_inits(_c, _n, _d, NULL);
-
-    cout << "Welcome to the decript program, please enter the encript: \r\n";
-    cin >> message;
-    cout << "Now enter the value of n: \r\n";
-    cin >> n;
-    cout << "Finally enter the value of d (private key): \r\n";
-    cin >> d;
-
-    long message_int = stoi(message);
-
-    mpz_ui_pow_ui(_c, message_int, d);
-    mpz_mod_ui(_c, _c, n);
-    unsigned char decrypt = mpz_get_ui(_c) +64;
-    cout << "The exact result is:" << (decrypt) << "\r\n";
+    unsigned char decrypt;
+    string text, numero;
+    getline(cin, text);
+    stringstream input_stringstream(text);
+    unsigned int buffer[10];
+    int i = 0;
+    while (getline(input_stringstream, numero, ','))
+    {
+        cout << "Un valor: " << stoi(numero) << endl;
+        buffer[i] = stoi(numero);
+        i++;
+    }
+    i = 0;
+    while (buffer[i])
+    {
+        mpz_ui_pow_ui(_c, buffer[i], 77);
+        mpz_mod_ui(_c, _c, 221);
+        if (mpz_get_ui(_c) != 27)
+        {
+            decrypt = mpz_get_ui(_c) + 64;
+        }
+        else
+        {
+            decrypt = 32;
+        }
+        cout << "The exact result is: " << (decrypt) << "\r\n";
+        i++;
+    }
+    return 0;
 }
--- test.cpp
+++ test.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sstream>
 #include <gmpxx.h>
 
 using namespace std;
@@ -6,6 +7,7 @@ using namespace std;
 int main(int argc, char **argv)
 {
 
+/*
     mpz_t a, b, c, d;
     mpz_inits(a, b, c, d,NULL);
 
@@ -17,6 +19,37 @@ int main(int argc, char **argv)
     cout << "\nThe exact result is:";
     mpz_out_str(stdout, 10, d); //Stream, numerical base, var
     cout << endl;
+*/
 
+    mpz_t _c, _n, _d;
+    mpz_inits(_c, _n, _d, NULL);
+    unsigned char decrypt;
+    string text, numero;
+    getline(cin, text);
+    stringstream input_stringstream(text);
+    unsigned int buffer[10];
+    int i = 0;
+    while (getline(input_stringstream, numero, ','))
+    {
+        cout << "Un valor: " << stoi(numero)<< endl;
+        buffer[i] = stoi(numero);
+        i++;
+    }
+    i=0;
+    while (buffer[i])
+    {
+        mpz_ui_pow_ui(_c, buffer[i], 77);
+        mpz_mod_ui(_c, _c, 221);
+        if(mpz_get_ui(_c) != 27)
+        {
+            decrypt = mpz_get_ui(_c) + 64;
+        }
+        else
+        {
+            decrypt = 32;
+        }
+        cout << "The exact result is: " << (decrypt) << "\r\n";
+        i++;
+    }
     return 0;
 }
